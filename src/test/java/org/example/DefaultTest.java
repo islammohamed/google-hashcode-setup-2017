f387999e4fe11f53decf56c07b8df753295f9561
Islam Mohamed <iabdelaziz@me.com>
2017-02-23 16:25:04 +0100

    first commit

---
+++ src/test/java/org/example/DefaultTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DefaultTest {
+    @Test
+    public void defaultTest() {
+        assertEquals("ocono", "ocono2");
+    }
+}
